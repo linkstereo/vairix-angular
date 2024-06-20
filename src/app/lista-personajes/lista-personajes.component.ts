3f3ff5921d7d04293def009e19e863d5ef5c2109
Andres Marin <linkstereo@gmail.com>
2024-06-20 00:45:13 -0300

    Se agrega el componente list-personajes

---
+++ src/app/lista-personajes/lista-personajes.component.ts
@@ -0,0 +1,27 @@
+import {Component, OnInit} from '@angular/core';
+import {RestApiClientService} from "../rest-api-clien-service/rest-api-client.service";
+import {Character} from "../dto/character";
+
+@Component({
+  selector: 'app-lista-personajes',
+  standalone: true,
+  imports: [],
+  templateUrl: './lista-personajes.component.html',
+  styleUrl: './lista-personajes.component.css'
+})
+export class ListaPersonajesComponent implements OnInit {
+    characters: Character[] = [];
+
+  constructor(private restApiClient: RestApiClientService) {
+  }
+
+
+  ngOnInit(): void {
+      var characters = this.restApiClient.getCharacters()
+        .subscribe( (data) => {
+          this.characters = data;
+          console.log(data);
+        });
+  }
+
+}
